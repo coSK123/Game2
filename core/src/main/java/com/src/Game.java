ae900ff28704fdeed487424327790720c9388004
coSK123 <skollmer03@gmail.com>
2023-05-28 22:57:16 +0200

    Implemented Logic for choosing a character

--- core/src/main/java/com/src/Game.java
+++ core/src/main/java/com/src/Game.java
@@ -9,13 +9,11 @@ import com.src.views.Startscreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Game extends com.badlogic.gdx.Game  {
-    public SpriteBatch batch;
-    public BitmapFont font;
+
 
     @Override
     public void create () {
-        batch = new SpriteBatch();
-        font = new BitmapFont(); // use libGDX's default Arial font
+
         this.setScreen(new Startscreen(this));
     }
 
@@ -27,7 +25,5 @@ public class Game extends com.badlogic.gdx.Game  {
 
     @Override
     public void dispose () {
-        batch.dispose();
-        font.dispose();
     }
 }
